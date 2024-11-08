634ab0f0a09dd8f31c67264ee65915cdf03ab4b3
Maksim <radchuk.maksime@gmail.com>
2024-11-08 10:56:30 +0300

    feature: dist

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path")
+module.exports={
+    mode:"production",
+    entry:{
+        filename:path.resolve(__dirname,"src/index.js") ,
+    },
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:"index.js"
+    }
+}
