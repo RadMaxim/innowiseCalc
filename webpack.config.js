07a2a6a22f8b184353bbf7864e61083cf0a7b1d8
Maksim <radchuk.maksime@gmail.com>
2024-11-08 23:42:55 +0300

    feature: test

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
-const path = require("path")
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+
 module.exports={
     mode:"production",
     entry:{
@@ -7,5 +12,27 @@ module.exports={
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:"index.js"
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "./index.html"),
+            filename: "index.html",
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new Dotenv(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 }
