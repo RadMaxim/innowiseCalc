f30e2bfa31620f7d17ec188fee8abd3f92d0daaf
Maksim <radchuk.maksime@gmail.com>
2024-11-11 13:32:37 +0300

    testing husky

--- src/js/states.js
+++ src/js/states.js
@@ -7,6 +7,7 @@ function updateStates(newStates) {
   console.log(newStates);
   console.log(newStates);
   console.log(newStates);
+  console.log(newStates);
   allState = { ...allState, ...newStates };
 }
 export { updateStates, allState };
